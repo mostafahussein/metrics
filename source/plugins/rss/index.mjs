008a5c84e289f467891734820646fdb8c9c94208
(╯°□°）╯︵ uᴉǝssnH ɐɟɐʇsoW <mostafa.hussein91@gmail.com>
2025-11-25 22:54:43 +0400

    chore: update headers
    Signed-off-by: (╯°□°）╯︵ uᴉǝssnH ɐɟɐʇsoW <mostafa.hussein91@gmail.com>

--- source/plugins/rss/index.mjs
+++ source/plugins/rss/index.mjs
@@ -17,8 +17,13 @@ export default async function({login, q, imports, data, account}, {enabled = fal
     //Set User-Agent header
     const parser = new rss({ //eslint-disable-line new-cap
       headers: {
-        "User-Agent": "gh-metrics/metrics (+https://github.com/gh-metrics/metrics)",
-        "Accept": "application/rss+xml, application/xml, text/xml, */*; q=0.1"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
       }
     })
 
